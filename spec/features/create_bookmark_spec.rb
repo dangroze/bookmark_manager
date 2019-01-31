e1dd13c9cf74337a281d5afdfc66ac4b962c3895
Dan Groze <dangroze@gmail.com>
2019-01-31 16:11:16 +0000

    12. Updated feature test for creating bookmarks

--- spec/features/create_bookmark_spec.rb
+++ spec/features/create_bookmark_spec.rb
@@ -2,8 +2,9 @@ feature 'Adding a new bookmark' do
   scenario 'User can add a bookmark to the Bookmark Manager' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://www.newbookmark.com')
+    fill_in('title', with: 'New Bookmark')
     click_button('Submit')
 
-    expect(page).to have_content('http://www.newbookmark.com')
+    expect(page).to have_link('New Bookmark', href: 'http://www.newbookmark.com')
   end
 end
