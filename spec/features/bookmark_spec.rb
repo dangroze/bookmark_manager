a777219978e4d562c3310bd8e538be5dbf35444c
Dan Groze <dangroze@gmail.com>
2019-01-31 16:18:03 +0000

    17. Added feature test

--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -2,18 +2,17 @@ require 'pg'
 
 feature 'Viewing bookmarks' do
   scenario 'can see a list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
     # Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com", title: "Makers")
+    Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy Software")
+    Bookmark.create(url: "http://www.google.com", title: "google")
 
     visit('/')
     click_button('Bookmarks')
 
     expect(page).to have_content 'List of Bookmarks'
-    expect(page).to have_content 'http://www.google.com'
-    expect(page).to have_content 'http://www.destroyallsoftware.com'
-    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_link('google', href: 'http://www.google.com')
+    expect(page).to have_link('Destroy Software', href: 'http://www.destroyallsoftware.com')
+    expect(page).to have_link('Makers', href: 'http://www.makersacademy.com')
   end
 end
