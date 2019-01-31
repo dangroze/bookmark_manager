a3e761316ebd9f1fbc31ff66703552c2bd175779
Dan Groze <dangroze@gmail.com>
2019-01-31 16:15:35 +0000

    15. Unit test for adding title to Bookmark.create

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,27 +1,28 @@
 require 'bookmark'
+require 'setup_test_database'
 
 RSpec.describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
       # Add test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
-
+      connection.exec("INSERT INTO bookmarks (title, url) VALUES ('Makers', 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (title, url) VALUES ('Destroy Software', 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (title, url) VALUES ('google', 'http://www.google.com');")
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include('http://www.google.com')
-      expect(bookmarks).to include('http://www.destroyallsoftware.com')
-      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks.first.title).to eq('Makers')
+      expect(bookmarks[1].title).to eq('Destroy Software')
+      expect(bookmarks[2].url).to eq('http://www.google.com')
     end
   end
 
   describe '.create' do
-  it 'creates a new bookmark' do
-    Bookmark.create(url: 'http://www.newbookmark.com')
+    it 'creates a new bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.newbookmark.com', title: 'New Bookmark').first
 
-    expect(Bookmark.all).to include 'http://www.newbookmark.com'
+      expect(bookmark['url']).to eq 'http://www.newbookmark.com'
+      expect(bookmark['title']).to eq 'New Bookmark'
+    end
   end
-end
 end
